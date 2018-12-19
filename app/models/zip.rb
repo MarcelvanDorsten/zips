44fe7694acfe7f8d01a12ba2eed1383d57959432
MarcelvanDorsten <info@asdonline.nl>
2018-12-19 01:10:46 +0100

    Coursera Rails-Mongodb 3.1 tm Full Modell Class

--- app/models/zip.rb
+++ app/models/zip.rb
@@ -1,4 +1,5 @@
 class Zip
+    include ActiveModel::Model
     
 attr_accessor :id, :city, :state, :population
     
@@ -6,7 +7,7 @@ def to_s
     "#{@id}: #{@city}, #{@state}, pop=#{@population}"
 end
     
-# initialize for both Mongo end a Web hash
+# initialize for both Mongo and a Web hash
 def initialize(params={})
     # switch between both internal and external views of id and population
     @id=params[:_id].nil? ? params[:id] : params[:_id]
@@ -15,15 +16,26 @@ def initialize(params={})
     @population=params[:pop].nil? ? params[:population] : params[:pop]
 end
         
-    # convenience method for access to client in console
-    def self.mongo_client
-        Mongoid::Clients.default
-    end
+# tells Rails whether tis instance is persisted
+def persisted?
+    !@id.nil
+end
+def created_at
+    nil
+end
+def updated_at
+    nil
+end
     
-    # convenience method for access to zips collection
-    def self.collection
-        self.mongo_client['zips']
-    end
+# convenience method for access to client in console
+def self.mongo_client
+    Mongoid::Clients.default
+end
+    
+# convenience method for access to zips collection
+def self.collection
+    self.mongo_client['zips']
+end
 
 # implement a find that returns a collection of documents as hashes
 # Use initiale(hash) to express individual documemts as a class instance
@@ -53,5 +65,44 @@ def self.all(prototype={}, sort={:population=>1}, offset=0, limit=100)
     return result
 end
     
+# locate a specific document. Use initialize(hash) on the result to get in class instance form.
+def self.find id
+    Rails.logger.debug {"getting zip #{id}"}
+    
+    doc=collection.find(:_id=>id)
+                .projection({_id:true, city:true, state:true, pop:true})
+                .first
+    return doc.nil? ? nil : Zip.new(doc)
+end
+
+# create a new document using the current instance    
+def save
+    Rails.logger.debug {"saving #{self}"}
+    
+    result=collection.insert_one(_id:@id, city:@city, state:@state, pop:@pop)
+    @id=result.inserted_id
+end
+    
+# updates the values for this instance
+def update(updates)
+    Rails.loggger.debug {"updating #{self} with #{updates}"}
+    
+    # map internal :population term to :pop document term
+    updates[:pop]=updates[:population] if !updates[:population].nil?
+    updates.slice!(:city, :state, :pop) if !updates.nil?
+    
+    self.class.collection
+              .find(_id:@id)
+              .update_one(:$set=>updates)
+end
+   
+# remove the document associated with this instance form the DB
+def destroy
+    Rails.logger.debug {"destroying #{self}"}
+    
+    self.class.collection
+              .find(_id:@id)
+              .delete.one
+end
     
 end
